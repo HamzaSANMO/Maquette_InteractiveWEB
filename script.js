ebc952eb0e393228ee5658aaf72ebf0c6690251c
Hamza SANMO <hamzasanmo@gmail.com>
2025-04-09 17:36:02 +0000

    new version of wednesday

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
+// declaration de fonction pour aller a la page cv
 function home(){
     window.location.href='cv.html'
 }
-// const btn = document.getElementById("download");
 
-function downloadPDF() {
-    const element = document.getElementById("cv");
-    html2pdf()
-      .from(element)
-      .save("cv.pdf");
-    //   .then(() => {
-    //     if (localStorage.getItem("count")) {
-    //       count = +localStorage.getItem("count");
-    //     }
-    //     count += 1;
-    //     localStorage.setItem("count", count);
-  
-    //     counter.innerText = localStorage.getItem("count");
-    //   });
+// declaration de fonction pour retour à la page d'acceuil
+function back(){
+    window.location.href='index.html'
 }
 
+// changer de couleur
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+     return color;
+  }
+    // Fonction pour changer la couleur du div
+    function changeColor() {
+      const colorDiv = document.getElementById('cv');
+      colorDiv.style.backgroundColor = getRandomColor();
+    }
+    document.getElementById('cvcolor').addEventListener('click', changeColor);
 
-// btn.addEventListener("click",()=>{
-//     console.log("fdsfdf");
-//   })
 
-const telecharger = document.getElementById('download')
-
-telecharger.addEventListener('click', ()=>{
-    console.log('telecharger');
-})
+    // description pour le telechargement du cv
+    document.getElementById("download").addEventListener("click", async () => {
+        const element = document.getElementById("cv");
+        
+     
+     
+      
+     
+       // Attendre que html2canvas fonctionne
+       const canvas = await html2canvas(element, {
+         useCORS: true,
+         allowTaint: false,
+         scale: 2, // qualité
+       });
+     
+       const imgData = canvas.toDataURL("image/png");
+     
+       // Créer le PDF
+       const { jsPDF } = window.jspdf;
+       const pdf = new jsPDF("p", "mm", "a4");
+     
+       const pageWidth = pdf.internal.pageSize.getWidth();
+       const pageHeight = pdf.internal.pageSize.getHeight();
+     
+       const imgProps = pdf.getImageProperties(imgData);
+       const pdfWidth = pageWidth;
+       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+     
+       pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+       pdf.save("cv_Hamza_SANMO.pdf");
+     
+      
+     
+     
+     
+     });
+   
